b83c216d52d432a48942ac261ca063664f38f18d
kawan idrees <kawanedres211@gmail.com>
2022-10-05 20:47:31 +0300

    this is a second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main  {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("helo kawan chony bashy ");
+        System.out.println("hello again ");
     }
 }
